89ef1e4671f2d7749089c394b610d3d50c771b86
berdikulov-571 <mygithub571@gmail.com>
2024-02-05 13:07:01 +0500

    routeing module ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './pages/card/card.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CardComponent },
+  { path: 'details/:id', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
