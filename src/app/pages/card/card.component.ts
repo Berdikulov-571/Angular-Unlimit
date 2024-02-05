fa7e573451779f0995e32692318e8579072d77f7
berdikulov-571 <mygithub571@gmail.com>
2024-02-05 17:53:53 +0500

    card component ts

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CardService } from './services/card.service';
 import { Router } from '@angular/router';
+import { CardData } from './interfaces/card-data';
 
 @Component({
   selector: 'app-card',
@@ -9,10 +10,15 @@ import { Router } from '@angular/router';
 })
 export class CardComponent {
 
+  filteredData!: CardData[];
+
   constructor(private cardService: CardService, private router: Router) {
+    this.cardService.getAllCardData().then(x => {
+      this.filteredData = x;
+    });
   }
 
-  filteredData = this.cardService.card;
+  // filteredData = this.cardService.getAllCardData();
 
   search(text: string) {
     if (text.length === 0)
