35aeacc35d1c3ccb2234b3ac427b767d2830ea9f
berdikulov-571 <mygithub571@gmail.com>
2024-02-05 17:54:26 +0500

    details component ts

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CardService } from '../card/services/card.service';
 import { CardData } from '../card/interfaces/card-data';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
@@ -12,9 +13,20 @@ export class DetailsComponent {
 
   card!: CardData | undefined;
 
+
+  form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    age: new FormControl(''),
+  });
+
+
+
   constructor(private route: ActivatedRoute, private cardService: CardService) {
     const wordId: string = this.route.snapshot.params['id']
     this.card = this.cardService.getCardById(+wordId);
   }
 
+  send() {
+    alert(`${this.form.value.name} ${this.form.value.age}`)
+  }
 }
